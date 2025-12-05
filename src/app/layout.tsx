161ff8a55b23c32fab36f28dc42856b7eef74146
yceffort <yceffort@gmail.com>
2025-12-05 20:19:49 +0900

    🗑️ Remove commented FloatingBanner from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,7 +96,6 @@ export default function Layout({children}: {children: ReactNode}) {
           <VercelAnalytics />
           <VercelSpeedInsights />
           <GoogleAnalyticsWebVitalsTracker />
-          {/* <FloatingBanner /> */}
         </body>
       </html>
     </>
