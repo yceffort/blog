cf046f631e7948b05dadbeb6ed76bede63dfc35e
yceffort <yceffort@gmail.com>
2025-12-05 20:19:38 +0900

    🗑️ Remove unused parseMarkdownToMdx function

--- src/utils/Markdown.ts
+++ src/utils/Markdown.ts
@@ -1,16 +1,6 @@
-import {serialize} from 'next-mdx-remote-client/serialize'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeKatex from 'rehype-katex'
-import prism from 'rehype-prism-plus'
-import slug from 'rehype-slug'
-import remarkGfm from 'remark-gfm'
-import remarkMath from 'remark-math'
-import toc from 'remark-toc'
 import {visit} from 'unist-util-visit'
 
 import type {Element, Root} from 'hast'
-
-import imageMetadataPlugin from '#utils/imageMetadata'
 
 type TokenType =
   | 'tag'
@@ -43,7 +33,9 @@ export function parseCodeSnippet() {
   return (tree: Root) => {
     visit(tree, 'element', (node: Element) => {
       const className = node.properties?.className
-      if (!Array.isArray(className)) {return}
+      if (!Array.isArray(className)) {
+        return
+      }
 
       const [token, type] = className as [string, TokenType]
       if (token === 'token' && type in tokenClassNames) {
@@ -51,22 +43,4 @@ export function parseCodeSnippet() {
       }
     })
   }
-}
-
-export async function parseMarkdownToMdx(body: string, path: string) {
-  return serialize({
-    source: body,
-    options: {
-      mdxOptions: {
-        remarkPlugins: [remarkMath, toc, slug, remarkGfm],
-        rehypePlugins: [
-          rehypeKatex,
-          prism,
-          parseCodeSnippet(),
-          rehypeAutolinkHeadings,
-          [imageMetadataPlugin, {path}],
-        ],
-      },
-    },
-  })
 }
