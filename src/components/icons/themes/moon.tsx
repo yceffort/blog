1c998b1fd0afd81703d09b23cc176f1b193a129b
yceffort <yceffort@gmail.com>
2025-12-05 19:44:36 +0900

    🔧 Remove @ts-ignore by moving suppressHydrationWarning to svg

--- src/components/icons/themes/moon.tsx
+++ src/components/icons/themes/moon.tsx
@@ -5,13 +5,9 @@ export default function Moon() {
       viewBox="0 0 20 20"
       fill="currentColor"
       className="text-gray-900 dark:text-gray-100"
+      suppressHydrationWarning
     >
-      <path
-        /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
-        // @ts-ignore
-        suppressHydrationWarning
-        d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
-      />
+      <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
     </svg>
   )
 }
