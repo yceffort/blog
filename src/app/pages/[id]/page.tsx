5f91b920334330c1774bff7a6391945668aa31aa
yceffort <yceffort@gmail.com>
2025-12-05 16:03:23 +0900

    ♻️ Redirect /pages/[id] to home page with query param

--- src/app/pages/[id]/page.tsx
+++ src/app/pages/[id]/page.tsx
@@ -1,7 +1,5 @@
-import {notFound} from 'next/navigation'
+import {redirect} from 'next/navigation'
 
-import ListLayout from '#components/layouts/ListLayout'
-import PageNumber from '#components/layouts/PageNumber'
 import {DEFAULT_NUMBER_OF_POSTS} from '#src/constants'
 import {getAllPosts} from '#utils/Post'
 
@@ -9,9 +7,7 @@ export const dynamic = 'error'
 
 export async function generateMetadata(props: {params: Promise<{id: string}>}) {
   const params = await props.params
-
   const {id} = params
-
   return {
     title: `Page ${id}`,
   }
@@ -19,7 +15,6 @@ export async function generateMetadata(props: {params: Promise<{id: string}>}) {
 
 export async function generateStaticParams() {
   const posts = await getAllPosts()
-
   return [
     ...new Array(Math.round(posts.length / DEFAULT_NUMBER_OF_POSTS)).keys(),
   ].map((i) => ({id: `${i + 1}`}))
@@ -27,36 +22,5 @@ export async function generateStaticParams() {
 
 export default async function Page(props: {params: Promise<{id: string}>}) {
   const params = await props.params
-  const allPosts = await getAllPosts()
-  const pageNo = parseInt(params.id)
-
-  if (
-    isNaN(pageNo) ||
-    pageNo > Math.ceil(allPosts.length / DEFAULT_NUMBER_OF_POSTS) ||
-    pageNo < 1
-  ) {
-    return notFound()
-  }
-
-  const startIndex = (pageNo - 1) * DEFAULT_NUMBER_OF_POSTS
-  const endIndex = startIndex + DEFAULT_NUMBER_OF_POSTS
-
-  const posts = allPosts.slice(startIndex, endIndex)
-
-  const hasNextPage =
-    Math.floor(allPosts.length / DEFAULT_NUMBER_OF_POSTS) > pageNo
-
-  const title = `Page ${pageNo}`
-
-  return (
-    <>
-      <ListLayout posts={posts} title={title} />
-      <PageNumber
-        pageNo={pageNo}
-        next={`/pages/${pageNo + 1}`}
-        prev={`/pages/${pageNo - 1}`}
-        hasNextPage={hasNextPage}
-      />
-    </>
-  )
+  redirect(`/?page=${params.id}`)
 }
