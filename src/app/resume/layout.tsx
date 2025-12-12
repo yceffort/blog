8772fb82708e027a562109610ff48b3de65f10f6
yceffort <yceffort@gmail.com>
2025-12-13 00:13:58 +0900

    💄 Apply prettier formatting to layout files

--- src/app/resume/layout.tsx
+++ src/app/resume/layout.tsx
@@ -1,18 +1,18 @@
-import type { ReactNode } from 'react'
-import type { Metadata } from 'next'
+import type {Metadata} from 'next'
+import type {ReactNode} from 'react'
 
-import { SiteConfig } from '#src/config'
+import {SiteConfig} from '#src/config'
 
 export const metadata: Metadata = {
   title: 'Resume - ' + SiteConfig.title,
-  description: 'Experienced Frontend-focused Fullstack Engineer\'s Resume',
+  description: "Experienced Frontend-focused Fullstack Engineer's Resume",
   openGraph: {
     title: 'Resume - ' + SiteConfig.title,
-    description: 'Experienced Frontend-focused Fullstack Engineer\'s Resume',
+    description: "Experienced Frontend-focused Fullstack Engineer's Resume",
     url: `${SiteConfig.url}/resume`,
     images: [
       {
-        url: `/api/og?title=${encodeURIComponent('Resume - ' + SiteConfig.title)}&description=${encodeURIComponent('Experienced Frontend-focused Fullstack Engineer\'s Resume')}&path=${encodeURIComponent('/resume')}&type=page`,
+        url: `/api/og?title=${encodeURIComponent('Resume - ' + SiteConfig.title)}&description=${encodeURIComponent("Experienced Frontend-focused Fullstack Engineer's Resume")}&path=${encodeURIComponent('/resume')}&type=page`,
         width: 1200,
         height: 630,
       },
