995be70df387f0f4c13500e443c7c0e1b2bb7373
yceffort <yceffort@gmail.com>
2025-12-12 14:59:43 +0900

    fix: resolve hydration mismatch in ThemeSwitch component

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import {useEffect, useState} from 'react'
+
 import {useTheme} from 'next-themes'
 
 import Moon from '#components/icons/themes/moon'
@@ -7,7 +9,13 @@ import Sun from '#components/icons/themes/sun'
 import {Theme} from '#constants/index'
 
 const ThemeSwitch = () => {
+  const [mounted, setMounted] = useState(false)
   const {theme, setTheme} = useTheme()
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setMounted(true)
+  }, [])
 
   function handleButtonClick() {
     setTheme(theme === Theme.dark ? Theme.light : Theme.dark)
@@ -20,7 +28,7 @@ const ThemeSwitch = () => {
       className="ml-1 mr-1 h-8 w-8 rounded-sm p-1 sm:ml-4"
       onClick={handleButtonClick}
     >
-      {theme === Theme.light ? <Sun /> : <Moon />}
+      {mounted && (theme === Theme.light ? <Sun /> : <Moon />)}
     </button>
   )
 }
