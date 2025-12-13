dca62cb90f3b75bde80ac4597bc640798b63db86
yceffort <yceffort@gmail.com>
2025-12-13 12:14:03 +0900

    📦 Export ThemeSwitch and SocialIcon from shared package

--- packages/shared/src/components/index.ts
+++ packages/shared/src/components/index.ts
@@ -1,1 +1,12 @@
 export {Providers} from './Provider'
+export {default as ThemeSwitch} from './ThemeSwitch'
+export {Sun, Moon, Monitor} from './icons/themes'
+export {default as SocialIcon} from './SocialIcon'
+export {
+  GithubIcon,
+  MailIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  FacebookIcon,
+  YoutubeIcon,
+} from './icons/social'
