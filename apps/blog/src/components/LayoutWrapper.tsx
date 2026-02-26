1a1ca08ce2b19c94c54d61d91b9ed224dbcfc401
yceffort <yceffort@gmail.com>
2026-02-26 16:13:36 +0900

    🔨 Integrate push button and SW registration in header

--- apps/blog/src/components/LayoutWrapper.tsx
+++ apps/blog/src/components/LayoutWrapper.tsx
@@ -12,7 +12,7 @@ import ProfileImage from './ProfileImage'
 import ScrollTop from './ScrollTop'
 import SectionContainer from './SectionContainer'
 
-import type {ReactNode} from 'react'
+import {useEffect, type ReactNode} from 'react'
 
 import {SiteConfig} from '#src/config'
 import {useLocale} from '#src/hooks/useLocale'
@@ -22,6 +22,10 @@ import LanguageSwitch from './LanguageSwitch'
 const DynamicThemeSwitch = dynamic(() => import('./ThemeSwitch'), {
   ssr: false,
   loading: () => <div className="ml-1 mr-1 h-10 w-10 rounded-md sm:ml-4" />,
+})
+
+const DynamicPushSubscribeButton = dynamic(() => import('./PushSubscribeButton'), {
+  ssr: false,
 })
 
 function HeaderLogo() {
@@ -63,6 +67,12 @@ function HeaderNav() {
 }
 
 function Header() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js')
+    }
+  }, [])
+
   return (
     <header
       className="sticky top-0 z-40 -mx-[calc(50vw-50%)] flex items-center justify-between px-[calc(50vw-50%)] py-6 backdrop-blur-md"
@@ -81,6 +91,7 @@ function Header() {
             <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z" />
           </svg>
         </a>
+        <DynamicPushSubscribeButton />
         <LanguageSwitch />
         <DynamicThemeSwitch />
         <MobileNav />
